9c155148b808f8aef6dd7966ace631d951500c1b
Hasnat Amjad <mhasnat647@gmail.com>
2025-09-10 11:03:54 +0500

    update

---
+++ assets/adv-product-grid.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hotspots = document.querySelectorAll(".hotspot-btn");
+  const modal = document.querySelector(".popup-modal-sl");
+  const overlay = document.querySelector(".popup-overlay-sl");
+  const closeBtn = document.querySelector(".popup-modal-sl .close-btn");
+
+  hotspots.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      modal.style.display = "block";
+      overlay.style.display = "block";
+      document.body.style.overflow = "hidden"; // stop scrolling
+    });
+  });
+
+  function closeModal() {
+    modal.style.display = "none";
+    overlay.style.display = "none";
+    document.body.style.overflow = ""; // restore scrolling
+  }
+
+  closeBtn.addEventListener("click", closeModal);
+  overlay.addEventListener("click", closeModal);
+});
