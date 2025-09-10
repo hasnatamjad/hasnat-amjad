699753a1d969cbe2a357dcc8e1b346993b9a8452
Hasnat Amjad <mhasnat647@gmail.com>
2025-09-10 16:04:38 +0500

    update

--- assets/adv-product-grid.js
+++ assets/adv-product-grid.js
@@ -1,23 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hotspots = document.querySelectorAll(".hotspot-btn");
-  const modal = document.querySelector(".popup-modal-sl");
-  const overlay = document.querySelector(".popup-overlay-sl");
-  const closeBtn = document.querySelector(".popup-modal-sl .close-btn");
+document.querySelectorAll(".advance-product-grid").forEach((root) => {
+  const hotspots = root.querySelectorAll(".hotspot-btn");
+  const modal = root.querySelector(".popup-modal-sl");
+  const overlay = root.querySelector(".popup-overlay-sl");
+  const closeBtn = root.querySelector(".popup-modal-sl .close-btn");
+  const innerContent = modal.querySelector(".popup-content ");
 
-  hotspots.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      modal.style.display = "block";
-      overlay.style.display = "block";
-      document.body.style.overflow = "hidden"; // stop scrolling
-    });
-  });
-
+  function showModal(html) {
+    modal.style.display = "block";
+    overlay.style.display = "block";
+    document.body.style.overflow = "hidden";
+    innerContent.innerHTML = html;
+  }
   function closeModal() {
     modal.style.display = "none";
     overlay.style.display = "none";
     document.body.style.overflow = "";
   }
 
+  hotspots.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const templateEL = document.getElementById(btn.dataset.target);
+      if (templateEL) showModal(templateEL.innerHTML);
+    });
+  });
   closeBtn.addEventListener("click", closeModal);
   overlay.addEventListener("click", closeModal);
 });
+
+document.addEventListener("DOMContentLoaded", function () {});
